2d756d6614e613a1b2d7dc927b831c4822333961
futuredonut <65640626+futuredonut@users.noreply.github.com>
2020-05-19 21:33:07 -0700

    new update

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ function tpsp(){
     document.getElementById("startpage").style.display = "inline";
     document.getElementById("tagpage").style.display = "none";
     document.getElementById("cmdpage").style.display = "none";
+    document.getElementById("videospage").style.display = "none";
 }
 
 function tptp(){
     document.getElementById("startpage").style.display = "none";
     document.getElementById("tagpage").style.display = "inline";
     document.getElementById("cmdpage").style.display = "none";
+    document.getElementById("videospage").style.display = "none";
 }
 
 function tpssp(){
     document.getElementById("startpage").style.display = "none";
     document.getElementById("tagpage").style.display = "none";
     document.getElementById("cmdpage").style.display = "inline";
+    document.getElementById("videospage").style.display = "none";
+}
+
+function tpvp(){
+    document.getElementById("startpage").style.display = "none";
+    document.getElementById("tagpage").style.display = "none";
+    document.getElementById("cmdpage").style.display = "none";
+    document.getElementById("videospage").style.display = "inline";
 }
 
 function test(){
