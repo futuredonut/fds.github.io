b1cfe70d276139834d2e54a6c6bb0b497a2a94b9
futuredonut <65640626+futuredonut@users.noreply.github.com>
2020-05-19 20:43:09 -0700

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+function tpsp(){
+    document.getElementById("startpage").style.display = "inline";
+    document.getElementById("tagpage").style.display = "none";
+    document.getElementById("cmdpage").style.display = "none";
+}
+
+function tptp(){
+    document.getElementById("startpage").style.display = "none";
+    document.getElementById("tagpage").style.display = "inline";
+    document.getElementById("cmdpage").style.display = "none";
+}
+
+function tpssp(){
+    document.getElementById("startpage").style.display = "none";
+    document.getElementById("tagpage").style.display = "none";
+    document.getElementById("cmdpage").style.display = "inline";
+}
+
+function test(){
+    console.log(`It is working!`)
+}
